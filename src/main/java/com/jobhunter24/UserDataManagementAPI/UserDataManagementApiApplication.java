2aa7373f6d8bdca62cbb534fc318432775e008d5
andrei.habas <habasescuandrei1234@gmail.com>
2025-01-25 20:30:33 +0200

    Repo Init

---
+++ src/main/java/com/jobhunter24/UserDataManagementAPI/UserDataManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.jobhunter24.UserDataManagementAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDataManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDataManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobhunter24/UserDataManagementAPI/UserDataManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobhunter24.UserDataManagementAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
